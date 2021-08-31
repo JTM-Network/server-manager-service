07fc899af68087944ec0e16dbeacea7d5adc411e
Joe Matthews <joetymatthews@gmail.com>
2021-08-31 19:16:59 +0100

    refactor: echo message received from socket

--- src/main/kotlin/com/jtm/server/entrypoint/configuration/SocketConfiguration.kt
+++ src/main/kotlin/com/jtm/server/entrypoint/configuration/SocketConfiguration.kt
@@ -1,23 +1,26 @@
 package com.jtm.server.entrypoint.configuration
 
 import com.jtm.server.entrypoint.socket.ServerSocketHandler
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
 import org.springframework.web.reactive.HandlerMapping
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
 import org.springframework.web.reactive.socket.WebSocketHandler
 
+
 @Configuration
 open class SocketConfiguration {
-
-    @Autowired
-    lateinit var serverSocketHandler: ServerSocketHandler
 
     @Bean
     open fun handlerMapping(): HandlerMapping {
         val map: MutableMap<String, WebSocketHandler> = HashMap()
-        map["/manager"] = serverSocketHandler
-        return SimpleUrlHandlerMapping(map, -1)
+        map["/manager"] = ServerSocketHandler()
+
+
+        val handlerMapping = SimpleUrlHandlerMapping()
+        handlerMapping.order = Ordered.HIGHEST_PRECEDENCE
+        handlerMapping.urlMap = map
+        return handlerMapping
     }
 }
--- src/main/kotlin/com/jtm/server/entrypoint/socket/ServerSocketHandler.kt
+++ src/main/kotlin/com/jtm/server/entrypoint/socket/ServerSocketHandler.kt
@@ -1,22 +1,15 @@
 package com.jtm.server.entrypoint.socket
 
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.socket.WebSocketHandler
-import org.springframework.web.reactive.socket.WebSocketMessage
 import org.springframework.web.reactive.socket.WebSocketSession
-import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Component
 class ServerSocketHandler: WebSocketHandler {
 
-    private val logger = LoggerFactory.getLogger(ServerSocketHandler::class.java)
-
     override fun handle(session: WebSocketSession): Mono<Void> {
-        val messages: Flux<WebSocketMessage> = session.receive()
-            .map { session.textMessage("Echo: ${it.payloadAsText}") }
-
-        return session.send(messages)
+        return session.send(session.receive()
+            .map { session.textMessage("Echo: ${it.payloadAsText}") })
     }
 }
