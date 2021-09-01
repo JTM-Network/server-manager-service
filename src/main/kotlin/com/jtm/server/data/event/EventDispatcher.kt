d2f59704f94313bffc29bf8b4eb8767da70a8a23
Joe Matthews <joetymatthews@gmail.com>
2021-09-01 17:14:01 +0100

    refactor: change return type to WebSocketMessage

--- src/main/kotlin/com/jtm/server/data/event/EventDispatcher.kt
+++ src/main/kotlin/com/jtm/server/data/event/EventDispatcher.kt
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.socket.CloseStatus
+import org.springframework.web.reactive.socket.WebSocketMessage
 import org.springframework.web.reactive.socket.WebSocketSession
 import reactor.core.publisher.Mono
 
@@ -14,10 +15,10 @@ class EventDispatcher @Autowired constructor(private val eventAggregator: EventA
 
     private val logger = LoggerFactory.getLogger(EventDispatcher::class.java)
 
-    fun dispatch(session: WebSocketSession, event: IncomingEvent): Mono<Void> {
+    fun dispatch(session: WebSocketSession, event: IncomingEvent): Mono<WebSocketMessage> {
         logger.info("Dispatching to correct event.")
-        if (!authenticationManager.authenticate(event.token)) return session.close(CloseStatus.create(1015, "Failed authentication."))
-        val handler = eventAggregator.getHandler(event.name) ?: return session.close(CloseStatus.create(1020, "Failed to find event."))
+        if (!authenticationManager.authenticate(event.token)) return session.close(CloseStatus.create(1015, "Failed authentication.")).then(Mono.empty())
+        val handler = eventAggregator.getHandler(event.name) ?: return session.close(CloseStatus.create(1020, "Failed to find event.")).then(Mono.empty())
         return handler.handleEvent(session, event)
     }
 }
--- src/main/kotlin/com/jtm/server/entrypoint/handler/PingEventHandler.kt
+++ src/main/kotlin/com/jtm/server/entrypoint/handler/PingEventHandler.kt
@@ -5,6 +5,7 @@ import com.jtm.server.core.domain.model.event.OutgoingEvent
 import com.jtm.server.core.domain.model.ping.PingPong
 import com.jtm.server.core.usecase.event.EventHandler
 import org.slf4j.LoggerFactory
+import org.springframework.web.reactive.socket.WebSocketMessage
 import org.springframework.web.reactive.socket.WebSocketSession
 import reactor.core.publisher.Mono
 
@@ -12,8 +13,8 @@ class PingEventHandler: EventHandler<PingPong>(PingPong::class.java) {
 
     private val logger = LoggerFactory.getLogger(ServerManagerService::class.java)
 
-    override fun onEvent(session: WebSocketSession, value: PingPong): Mono<Void> {
+    override fun onEvent(session: WebSocketSession, value: PingPong): Mono<WebSocketMessage> {
         logger.info("Message received: ${value.value}")
-        return sendEvent(session, OutgoingEvent("pong"), PingPong("pong"))
+        return sendMessage(session, OutgoingEvent("pong"), PingPong("pong"))
     }
 }
