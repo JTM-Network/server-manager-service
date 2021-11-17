cbb92d814bac086687570a228f394cad3c5b7975
Joe Matthews <joetymatthews@gmail.com>
2021-11-17 21:17:39 +0000

    refactor: fix finding directory.

--- src/main/kotlin/com/jtm/server/core/domain/entity/Directory.kt
+++ src/main/kotlin/com/jtm/server/core/domain/entity/Directory.kt
@@ -1,7 +1,9 @@
 package com.jtm.server.core.domain.entity
 
+import com.jtm.server.ServerManagerService
 import com.jtm.server.core.domain.dto.DirectoryDto
 import com.jtm.server.core.domain.model.File
+import org.slf4j.LoggerFactory
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.util.*
@@ -9,12 +11,19 @@ import java.util.*
 @Document("directories")
 data class Directory(@Id val serverId: UUID, val name: String, val directories: MutableList<Directory> = mutableListOf(), val files: MutableList<File> = mutableListOf()) {
 
+    private val logger = LoggerFactory.getLogger(ServerManagerService::class.java)
+
     constructor(serverId: UUID, dto: DirectoryDto): this(serverId = serverId, name = dto.name, directories = dto.directories, files = dto.files)
 
     fun findDirectory(path: String): Directory {
         val folders = path.split("/")
         var dir = this
-        for (folder in folders) dir = dir.getDirectory(folder)
+        for (folder in folders) {
+            if (folder.isNotBlank()) {
+                logger.info("Folder: $folder")
+                dir = dir.getDirectory(folder)
+            }
+        }
         return dir
     }
 
