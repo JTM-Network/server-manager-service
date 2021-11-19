e4d8591bb0aa7bfb0068e48286831b9bf07d0a97
Joe Matthews <joetymatthews@gmail.com>
2021-11-19 04:35:39 +0000

    refactor: fix file object with information

--- src/main/kotlin/com/jtm/server/core/domain/model/File.kt
+++ src/main/kotlin/com/jtm/server/core/domain/model/File.kt
@@ -1,3 +1,3 @@
 package com.jtm.server.core.domain.model
 
-data class File(val name: String)
+data class File(val name: String, val size: Long, val lastModified: Long, val path: String, val format: String)
