ed68a9ceaec77a270228729cc10187edff44176e
Joe Matthews <joetymatthews@gmail.com>
2021-11-19 04:10:54 +0000

    new: test dispatch command

--- src/main/kotlin/com/jtm/server/entrypoint/handler/FetchDirectoryHandler.kt
+++ src/main/kotlin/com/jtm/server/entrypoint/handler/FetchDirectoryHandler.kt
@@ -5,6 +5,7 @@ import com.jtm.server.core.domain.entity.Directory
 import com.jtm.server.core.domain.model.event.impl.FetchDirectoryEvent
 import com.jtm.server.core.usecase.event.EventHandler
 import com.jtm.server.data.service.DirectoryService
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.socket.WebSocketMessage
@@ -15,9 +16,17 @@ import java.util.*
 @Component
 class FetchDirectoryHandler @Autowired constructor(private val directoryService: DirectoryService): EventHandler<FetchDirectoryEvent>("fetch_directory", FetchDirectoryEvent::class.java) {
 
+    private val logger = LoggerFactory.getLogger(FetchDirectoryHandler::class.java)
+
     override fun onEvent(session: WebSocketSession, value: FetchDirectoryEvent): Mono<WebSocketMessage> {
-        return directoryService.addDirectory(constructDirectory(value.serverId, value.directory))
-                .flatMap { Mono.empty() }
+        logger.info("Fetch directory here.")
+        return try {
+            directoryService.addDirectory(constructDirectory(value.serverId, value.directory))
+                    .flatMap { Mono.empty() }
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+            Mono.empty()
+        }
     }
 
     private fun constructDirectory(serverId: UUID, dto: DirectoryDto): Directory {
