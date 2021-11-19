5f7333e47fd105112ca76fa11d28838913d9f0da
Joe Matthews <joetymatthews@gmail.com>
2021-11-19 22:12:33 +0000

    refactor: add new plugin information.

--- src/main/kotlin/com/jtm/server/core/domain/model/plugin/Plugin.kt
+++ src/main/kotlin/com/jtm/server/core/domain/model/plugin/Plugin.kt
@@ -1,3 +1,3 @@
 package com.jtm.server.core.domain.model.plugin
 
-data class Plugin(val name: String)
+data class Plugin(val name: String, val description: String?, val enabled: Boolean, val version: String, val apiVersion: String?, val authors: List<String>, val depend: List<String>, val softDepend: List<String>)
