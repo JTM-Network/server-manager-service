4f70b112bd739f18e41d9fd63ddd73f54d60cc4c
Joe Matthews <joetymatthews@gmail.com>
2021-11-19 04:46:04 +0000

    refactor: add format field to file

--- src/main/kotlin/com/jtm/server/core/domain/model/File.kt
+++ src/main/kotlin/com/jtm/server/core/domain/model/File.kt
@@ -1,3 +1,3 @@
 package com.jtm.server.core.domain.model
 
-data class File(val name: String, val size: Long, val lastModified: Long)
+data class File(val name: String, val size: Long, val lastModified: Long, val format: String)
