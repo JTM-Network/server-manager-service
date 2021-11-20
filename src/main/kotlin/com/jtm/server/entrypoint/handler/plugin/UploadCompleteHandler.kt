3ea97c5a551404cd73925730e88e1852c959d3dd
Joe Matthews <joetymatthews@gmail.com>
2021-11-20 21:47:40 +0000

    refactor: log download request update.

--- src/main/kotlin/com/jtm/server/entrypoint/handler/plugin/UploadCompleteHandler.kt
+++ src/main/kotlin/com/jtm/server/entrypoint/handler/plugin/UploadCompleteHandler.kt
@@ -5,6 +5,7 @@ import com.jtm.server.core.domain.model.event.impl.plugin.UploadCompleteEvent
 import com.jtm.server.core.usecase.event.EventHandler
 import com.jtm.server.core.usecase.repository.DownloadRequestRepository
 import com.jtm.server.core.usecase.sink.RequestSink
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.socket.WebSocketMessage
@@ -13,13 +14,15 @@ import reactor.core.publisher.Mono
 
 @Component
 class UploadCompleteHandler @Autowired constructor(private val requestRepository: DownloadRequestRepository, private val requestSink: RequestSink): EventHandler<UploadCompleteEvent>("upload_complete", UploadCompleteEvent::class.java) {
+
+    private val logger = LoggerFactory.getLogger(UploadCompleteHandler::class.java)
+
     override fun onEvent(session: WebSocketSession, value: UploadCompleteEvent): Mono<WebSocketMessage> {
+        logger.info("Download request update received.")
         return requestRepository.findById(value.id)
                 .flatMap { requestRepository.save(it.updateStatus(DownloadStatus.DOWNLOAD_AVAILABLE))
-                        .flatMap { request ->
-                            requestSink.sendMessage(request)
-                            Mono.empty()
-                        }
+                        .flatMap { Mono.just(requestSink.sendMessage(it)) }
+                        .then(Mono.empty())
                 }
     }
 }
