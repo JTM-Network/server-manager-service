2ab9ec52935b0f6ad159daf614a7a6c80935d8f7
Joe Matthews <joetymatthews@gmail.com>
2021-11-23 02:12:58 +0000

    refactor: add directory event handling.

--- src/main/kotlin/com/jtm/server/core/domain/model/event/impl/fetch/FetchDirectoryEvent.kt
+++ src/main/kotlin/com/jtm/server/core/domain/model/event/impl/fetch/FetchDirectoryEvent.kt
@@ -3,4 +3,4 @@ package com.jtm.server.core.domain.model.event.impl.fetch
 import com.jtm.server.core.domain.dto.DirectoryDto
 import java.util.*
 
-data class FetchDirectoryEvent(val name: String, val serverId: UUID, val directory: DirectoryDto)
+data class FetchDirectoryEvent(val name: String, val directory: DirectoryDto)
--- src/main/kotlin/com/jtm/server/entrypoint/handler/FetchDirectoryHandler.kt
+++ src/main/kotlin/com/jtm/server/entrypoint/handler/FetchDirectoryHandler.kt
@@ -3,6 +3,7 @@ package com.jtm.server.entrypoint.handler
 import com.jtm.server.core.domain.dto.DirectoryDto
 import com.jtm.server.core.domain.entity.Directory
 import com.jtm.server.core.domain.entity.DirectoryEntity
+import com.jtm.server.core.domain.model.event.impl.directory.AddedDirectoryEvent
 import com.jtm.server.core.domain.model.event.impl.fetch.FetchDirectoryEvent
 import com.jtm.server.core.usecase.event.EventHandler
 import com.jtm.server.data.service.directory.DirectoryService
@@ -18,8 +19,8 @@ import java.util.*
 class FetchDirectoryHandler @Autowired constructor(private val directoryService: DirectoryService): EventHandler<FetchDirectoryEvent>("fetch_directory", FetchDirectoryEvent::class.java) {
 
     override fun onEvent(session: WebSocketSession, value: FetchDirectoryEvent): Mono<WebSocketMessage> {
-        return directoryService.addDirectory(constructDirectory(value.serverId, value.directory))
-                .then(Mono.empty())
+        return directoryService.addDirectory(constructDirectory(UUID.fromString(value.directory.serverId), value.directory))
+                .flatMap { sendMessage("added_directory", session, AddedDirectoryEvent(value.name)) }
     }
 
     private fun constructDirectory(serverId: UUID, dto: DirectoryDto): DirectoryEntity {
